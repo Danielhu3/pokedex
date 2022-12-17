eeeb5ae4f382e866eb3512e88e3e465b3cd9e6b1
Danielhu3 <danielhu3123@gmail.com>
2022-12-17 16:38:40 -0300

    search bar incomplete css

--- src/components/searchBar/index.tsx
+++ src/components/searchBar/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { SearchBar } from './style'
 
-const index = () => {
+const Index = () => {
+  const [value, setValue] = React.useState('')
+
+
   return (
-    <SearchBar></SearchBar>
+    <SearchBar placeholder='Search' value={value} onChange={(e)=> setValue(e.target.value)}></SearchBar>
   )
 }
 
-export default index
+export default Index
--- src/components/searchBar/style.ts
+++ src/components/searchBar/style.ts
@@ -1,2 +1,17 @@
 import styled from "styled-components";
-export const SearchBar = styled.input``
+import iconSearch from '../../../public/images/icon-pokeball.svg'
+
+export const SearchBar = styled.input.attrs((props: {value: string}) => props)`
+
+    border: solid 1px #E0E0E0;
+    border-radius: 0.8rem;
+    outline: none;
+
+    padding: 0.4rem 0;
+
+    text-align: center;
+    
+  
+
+    
+`
